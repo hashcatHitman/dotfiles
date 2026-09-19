87783b4f30b3713def1e2d44e36fee25925d7c73
hashcatHitman <155700084+hashcatHitman@users.noreply.github.com>
2026-09-19 20:32:06 +0000

    feat(bash): Export `DO_NOT_TRACK=1`
    Signed-off-by: hashcatHitman <155700084+hashcatHitman@users.noreply.github.com>

--- .bash_modules/exports.bash
+++ .bash_modules/exports.bash
@@ -12,5 +12,8 @@ export PYTHONPYCACHEPREFIX="$HOME/.cache/__pycache__"
 # Set JAVA_HOME
 export JAVA_HOME=/usr/lib/jvm/java-latest-openjdk/
 
+# Some applications respect this as a way to disable telemetry.
+export DO_NOT_TRACK=1
+
 # Load cargo env
 source "$HOME/.cargo/env"
