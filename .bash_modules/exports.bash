36f407cb1e17e3a65ef7c7d7c72e5f635cd27419
hashcatHitman <155700084+hashcatHitman@users.noreply.github.com>
2026-09-19 20:35:26 +0000

    feat(bash): Export `GH_TELEMETRY=0`
    Signed-off-by: hashcatHitman <155700084+hashcatHitman@users.noreply.github.com>

--- .bash_modules/exports.bash
+++ .bash_modules/exports.bash
@@ -15,5 +15,11 @@ export JAVA_HOME=/usr/lib/jvm/java-latest-openjdk/
 # Some applications respect this as a way to disable telemetry.
 export DO_NOT_TRACK=1
 
+# The GitHub CLI respects `DO_NOT_TRACK` at the time of writing. But just in
+# case they remove support for some reason... this also disables telemetry for
+# the GitHub CLI.
+# https://docs.github.com/en/github-cli/github-cli/github-cli-telemetry#how-to-opt-out
+export GH_TELEMETRY=0
+
 # Load cargo env
 source "$HOME/.cargo/env"
