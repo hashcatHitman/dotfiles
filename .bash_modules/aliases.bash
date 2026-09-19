8f5789df165f493efa1705772ac3939c4e4cc257
hashcatHitman <155700084+hashcatHitman@users.noreply.github.com>
2026-09-19 22:54:11 +0000

    feat(bash): Add an alias for `eza`
    Signed-off-by: hashcatHitman <155700084+hashcatHitman@users.noreply.github.com>

--- .bash_modules/aliases.bash
+++ .bash_modules/aliases.bash
@@ -2,3 +2,17 @@
 
 # Set the timezone offset to +0000 for git commands.
 alias git='TZ=UTC0 git'
+
+# `eza` how I like it.
+#
+# Display:
+# -F does things like ending directories with `/` and sym links with `@`
+#
+# Sorting:
+# --group-directories-first lists directories first. Duh!
+#
+# Only applicable for eza -l (long view):
+# -b shows file sizes with binary prefixes (KiB, etc)
+# -h adds a header
+# --smart-group only shows the group if the name is different than the owner
+alias eza='eza -F -bh --group-directories-first --smart-group'
